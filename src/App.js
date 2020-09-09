e4b144dcdc1a8198df567cf26ce4793fe8e560b0
Rozi <roza.ghazaryan.9@gmail.com>
2020-09-09 11:45:17 +0400

    change modules to pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import './assets/style/index.scss';
 import 'antd/dist/antd.css';
-
+import './assets/style/index.scss';
 import { BrowserRouter } from 'react-router-dom';
-import RegRequest from "./modules/reg-request/RegRequest";
-import NewSubscriber from "./modules/new-subscriber/NewSubscriber";
-import CustomerInfo from "./modules/customer-info/CustomerInfo";
-import CustomersList from "./modules/customers-list/CustomersList";
+import RegRequest from "./pages/reg-request/RegRequest";
+import NewSubscriber from "./pages/new-subscriber/NewSubscriber";
+import CustomerInfo from "./pages/customer-info/CustomerInfo";
+import CustomersList from "./pages/customers-list/CustomersList";
 
 
 function App() {
   return (
     <BrowserRouter history={window.routerHistory}>
       <div className='app'>
-        {/* <RegRequest /> */}
-        <NewSubscriber />
+        <RegRequest />
+        {/* <NewSubscriber /> */}
         {/* <CustomerInfo /> */}
         {/* <CustomersList /> */}
       </div>
